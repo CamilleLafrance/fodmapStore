e7037d8bba41eb1fdfcb930c74e2577244bc470a
CamilleLafrance <cam.lafr@free.fr>
2023-08-03 11:14:05 +0200

    create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProductsComponent } from './products/products.component';
+import { UserComponent } from './user/user.component';
+import { ServicesComponent } from './products/services/services.component';
+import { CartComponent } from './products/cart/cart.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ProductsComponent,
+    UserComponent,
+    ServicesComponent,
+    CartComponent,
+    AdminProductsComponent,
+    AdminUsersComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
