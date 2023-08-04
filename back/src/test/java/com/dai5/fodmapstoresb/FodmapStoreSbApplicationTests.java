87e0be6d84b09ea5153f7a1bc1309c4eb6a7e807
CamilleLafrance <cam.lafr@free.fr>
2023-08-04 09:48:38 +0200

    update repo

---
+++ back/src/test/java/com/dai5/fodmapstoresb/FodmapStoreSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dai5.fodmapstoresb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FodmapStoreSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ front/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProductsComponent } from './products/products.component';
+import { UserComponent } from './user/user.component';
+import { ServicesComponent } from './products/services/services.component';
+import { CartComponent } from './products/cart/cart.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    ProductsComponent,
+    UserComponent,
+    ServicesComponent,
+    CartComponent,
+    AdminProductsComponent,
+    AdminUsersComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
