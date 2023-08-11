ac4c208ec854b65f00b36dcafc9987437fd4d53d
CamilleLafrance <cam.lafr@free.fr>
2023-08-11 10:49:42 +0200

    add methods update & delete to product service

--- back/src/main/java/com/dai5/back/service/product/ProductService.java
+++ back/src/main/java/com/dai5/back/service/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.dai5.back.service.product;
 
+import com.dai5.back.model.product.Category;
 import com.dai5.back.model.product.Product;
 import com.dai5.back.repository.product.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +14,37 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    // CREATE
     public Product create(Product product){
         return this.productRepository.save(product);
     }
 
+    // READ
     public List<Product> getAll() {
         return this.productRepository.findAll();
     }
 
     public Product getById(Integer id) {
         return this.productRepository.findById(id).orElse(null);
+    }
+
+    // UPDATE
+    public Product update(Integer id, Product productDetails) {
+        Product product = productRepository.findById(id).get();
+        product.setBarCode(productDetails.getBarCode());
+        product.setName(productDetails.getName());
+        product.setDescription(productDetails.getDescription());
+        product.setImage(productDetails.getImage());
+        product.setPrice(productDetails.getPrice());
+        product.setCategory(productDetails.getCategory());
+        product.setStockQuantity(product.getStockQuantity());
+
+        return productRepository.save(product);
+    }
+
+    // DELETE
+    public void delete(Integer id) {
+        productRepository.deleteById(id);
     }
 
 }
