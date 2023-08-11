0cd8c67836e74fc8408f68219f0cfb5261214f50
CamilleLafrance <cam.lafr@free.fr>
2023-08-11 11:19:08 +0200

    add methods update & delete to order service

--- back/src/main/java/com/dai5/back/service/order/OrderService.java
+++ back/src/main/java/com/dai5/back/service/order/OrderService.java
@@ -1,6 +1,7 @@
 package com.dai5.back.service.order;
 
 import com.dai5.back.model.order.Order;
+import com.dai5.back.model.product.Category;
 import com.dai5.back.repository.order.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,35 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    // CREATE
     public Order create(Order order){
         return this.orderRepository.save(order);
     }
 
+    // READ
     public List<Order> getAll() {
         return this.orderRepository.findAll();
     }
 
     public Order getById(Integer id) {
         return this.orderRepository.findById(id).orElse(null);
+    }
+
+    // UPDATE
+    public Order update(Integer id, Order orderDetails) {
+        Order order = orderRepository.findById(id).get();
+        order.setUser(orderDetails.getUser());
+        order.setTotalPrice(orderDetails.getTotalPrice());
+        order.setOrderStatus(orderDetails.getOrderStatus());
+        order.setShippedAt(orderDetails.getShippedAt());
+        order.setDeliveredAt(orderDetails.getDeliveredAt());
+
+        return orderRepository.save(order);
+    } 
+
+    // DELETE
+    public void delete(Integer id) {
+        orderRepository.deleteById(id);
     }
 
 }
