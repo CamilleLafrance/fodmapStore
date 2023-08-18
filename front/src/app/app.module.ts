bc5fe10c9aec66eaa36268dde73b134c6884aca1
CamilleLafrance <cam.lafr@free.fr>
2023-08-18 12:02:27 +0200

    strengthen navbar from homepage

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomepageComponent } from './components/homepage/homepage.component';
-import { CategoryComponent } from './components/category/category.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { OrderComponent } from './components/order/order.component';
-import { UserComponent } from './components/user/user.component';
+import { HomepageComponent } from "./components/homepage/homepage.component";
+import { CategoryComponent } from "./components/category/category.component";
+import { ProductComponent } from "./components/product/product.component";
+import { OrderComponent } from "./components/order/order.component";
+import { UserComponent } from "./components/user/user.component";
+import { ProductPageComponent } from "./product-page/product-page.component";
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, CategoryComponent, ProductComponent, ProductDetailsComponent, OrderComponent, UserComponent],
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    CategoryComponent,
+    ProductComponent,
+    OrderComponent,
+    UserComponent,
+    ProductPageComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- front/src/app/components/product/product.component.ts
+++ front/src/app/components/product/product.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"],
 })
-export class ProductComponent {
-
-}
+export class ProductComponent {}
