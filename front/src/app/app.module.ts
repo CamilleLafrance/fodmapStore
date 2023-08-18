dd96c07a3266a1884f022d9d61e0b7c82d3ea737
CamilleLafrance <cam.lafr@free.fr>
2023-08-18 11:31:34 +0200

    create components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { OrderComponent } from './components/order/order.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomepageComponent, CategoryComponent, ProductComponent, ProductDetailsComponent, OrderComponent, UserComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
