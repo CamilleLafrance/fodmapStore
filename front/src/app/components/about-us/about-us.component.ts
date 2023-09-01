edb5644df9286b29305ac0f310e49a0e0b40f34d
CamilleLafrance <cam.lafr@free.fr>
2023-09-01 16:28:00 +0200

    improve forms contact, sign up & sign in

--- front/src/app/components/about-us/about-us.component.ts
+++ front/src/app/components/about-us/about-us.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-about-us',
-  templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  selector: "app-about-us",
+  templateUrl: "./about-us.component.html",
+  styleUrls: ["./about-us.component.css"],
 })
 export class AboutUsComponent {
+  public contactForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: this.formBuilder.control(""),
+      email: ["", [Validators.required, Validators.email]],
+      message: this.formBuilder.control(""),
+    });
+  }
+
+  sendMessage(): void {
+    alert("Message envoyé avec succès !");
+    this.router.navigate(["main-view"]);
+  }
 }
